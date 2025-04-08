9eec9a95f9026738574fe340850109ba00129cf0
dakshilkanakia1 <drkanaki@ncsu.edu>
2025-04-08 14:35:40 -0400

    renamed to opals

--- OPALS/asgi.py
+++ OPALS/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for POEMS project.
+ASGI config for OPALS project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'POEMS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OPALS.settings')
 
 application = get_asgi_application()
--- OPALS/wsgi.py
+++ OPALS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for POEMS project.
+WSGI config for OPALS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'POEMS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OPALS.settings')
 
 application = get_wsgi_application()
