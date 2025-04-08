bb0a6fb4f038d7514d5dbbd5f1a465652e17b518
dakshilkanakia1 <drkanaki@ncsu.edu>
2025-04-08 14:33:45 -0400

    renamed to opals

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import os
+import numpy as np
+import pandas as pd
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def page1(request):
+    return render(request, 'page1.html', {})
